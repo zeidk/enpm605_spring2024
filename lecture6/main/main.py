df0f203c981a8ca1f98ee7d09b446dc6da2ec244
zeidk <zeidk@umd.edu>
2024-03-05 13:09:06 -0500

    Final version

---
+++ lecture6/main/main.py
@@ -0,0 +1,28 @@
+import sys
+import os.path
+folder = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(folder)
+# Import your functions
+
+from rpg.player import Player  # noqa: E402
+from rpg.enemy import Enemy  # noqa: E402
+import random  # noqa: E402
+# from lecture6.rpg.enemy import Enemy
+# from lecture6.rpg.maze import Maze
+# from lecture6.rpg.item import Item
+
+if __name__ == "__main__":
+    # Create a player
+    
+    player = Player("Link", 100)
+    enemy = Enemy("Ganon", 100)
+    game_action = [player.attack, enemy.attack]
+    
+    while player.health > 0:
+        action = random.choice(game_action)
+        if action == player.attack:
+            action(enemy, 20)
+        elif action == enemy.attack:
+            action(player, 30)
+        else:
+            print("Invalid action")
